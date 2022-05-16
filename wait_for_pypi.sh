007c59aa209772f2dcfc0eaf1b8d60626259df35
Martin Roed <msroed@gmail.com>
2022-05-16 22:12:39 +0200

    First

---
+++ wait_for_pypi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eux
+
+PACKAGE_NAME=$1
+PACKAGE_VERSION=$2
+TIMEOUT=$3
+DELAY_BETWEEN_REQUESTS=$4
+URL=https://pypi.org/project/$PACKAGE_NAME/$PACKAGE_VERSION/
+HOST=$HOST STATUS=200 timeout \
+  --foreground -s TERM $TIMEOUT bash -c \
+    'while [[ ${STATUS_RECEIVED} != ${STATUS} ]];\
+        do STATUS_RECEIVED=$(curl -s -o /dev/null -L -w ''%{http_code}'' ${URL}) && \
+        echo "received status: $STATUS_RECEIVED" && \
+        sleep $DELAY_BETWEEN_REQUESTS;\
+    done;
+    echo success with status: $STATUS_RECEIVED'
